9844f013e3f39f772b31d8d2e73c5a0439fc044d
Blake Pond <blakehp@icloud.com>
2022-01-05 18:51:36 -0700

    trying to fix the overflow

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,26 +3,34 @@ import "./about.css";
 import Build from "../../img/ratemybuild.jpeg";
 
 const About = () => {
-  return (
-    <div className="a">
-      <div className="a-left">
-        <div className="a-card bg"></div>
-        <div className="a-card">
-          <img src={Build} alt="" className="a-image" />
-        </div>
+   return (
+      <div className="a">
+         <div className="a-left">
+            <div className="a-card bg"></div>
+            <div className="a-card">
+               <img
+                  src={Build}
+                  alt=""
+                  className="a-image"
+               />
+            </div>
+         </div>
+         <div className="a-right">
+            <div className="a-bg"></div>
+            <h1 className="a-title">
+               Who's the audience?
+            </h1>
+            <p className="a-desc">
+               Technicians Business Artists
+               Accountants Designers.
+               <br />
+               Media Developers Music
+               Entertainment Law Infracture.
+            </p>
+            <div className="a-bg"></div>
+         </div>
       </div>
-      <div className="a-right">
-        <div className="a-bg"></div>
-        <h1 className="a-title">who's the audience?</h1>
-        <p className="a-desc">
-          Technicians.Business.Artists.Accountants.Designers.
-          <br />
-          Media.Developers.Music.Entertainment.Law.Infracture.
-        </p>
-        <div className="a-bg"></div>
-      </div>
-    </div>
-  );
+   );
 };
 
 export default About;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,36 +1,54 @@
 import React from "react";
 import "./intro.css";
-import Profile from '../../img/IMG_0437.png'
+import Profile from "../../img/IMG_0437.png";
 
 const Intro = () => {
-  return (
-    <div className="i">
-      <div className="i-left">
-        <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello, I'm </h2>
-          <h3 className="i-name">blake pond</h3>
-          <div className="i-title">
-            <div className="i-title-wrapper">
-              <div className="i-title-item">mountain man</div>
-              <div className="i-title-item">web developer</div>
-              <div className="i-title-item">ui/ux designer</div>
-              <div className="i-title-item">entrepreneur</div>
-              <div className="i-title-item">musician</div>
+   return (
+      <div className="i">
+         <div className="i-left">
+            <div className="i-left-wrapper">
+               <h2 className="i-intro">
+                  Hello, I'm{" "}
+               </h2>
+               <h3 className="i-name">
+                  Blake Pond
+               </h3>
+               <div className="i-title">
+                  <div className="i-title-wrapper">
+                     <div className="i-title-item">
+                        mountain man
+                     </div>
+                     <div className="i-title-item">
+                        web developer
+                     </div>
+                     <div className="i-title-item">
+                        ui/ux designer
+                     </div>
+                     <div className="i-title-item">
+                        entrepreneur
+                     </div>
+                     <div className="i-title-item">
+                        musician
+                     </div>
+                  </div>
+               </div>
+               {/* <p className="i-desc">This is my personal brand.</p> */}
+               {/* <p className="i-desc">Collaboration starts here.</p> */}
+               {/* <p className="i-q">
+                  innovate or collaborate
+               </p> */}
             </div>
-          </div>
-          {/* <p className="i-desc">This is my personal brand.</p> */}
-          {/* <p className="i-desc">Collaboration starts here.</p> */}
-          <p className="i-q">innovate or collaborate</p>
-   
-        </div>
+         </div>
+         <div className="i-right">
+            <div className="i-bg"></div>
+            <img
+               className="i-image"
+               src={Profile}
+               alt=""
+            />
+         </div>
       </div>
-      <div className="i-right">
-        <div className="i-bg">
-        </div>
-          <img className="i-image" src={Profile} alt="" />
-      </div>
-    </div>
-  );
+   );
 };
 
 export default Intro;
